5c2f3a0bf02c07ff0e5824c6db1161fc71718e7d
SebJez <sjezierski123@gmail.com>
2019-06-03 17:48:46 +0200

    MainMenu actions return pointers to current menu

--- SliderSoftware.ino
+++ SliderSoftware.ino
@@ -1,11 +1,12 @@
 #include "definitions.h"
 #include "lcd16x2.h"
 #include "motor.h"
+#include "menu.h"
+#include <Encoder.h>
 
 #ifdef REMOTE_SHUTTER
     #include "shutter.h"
 #endif //REMOTE_SHUTTER
-
 
 Motor stepper = Motor(PIN_STEPPER_A1, PIN_STEPPER_A2, PIN_STEPPER_B1, PIN_STEPPER_B2,\
   STEPS_PER_MM, DEFAULT_SPEED, FLIP_DIRECTION, ENABLE_ENDSTOPS, MAX_STEPS);
@@ -18,13 +19,46 @@ Shutter shutter = Shutter(PIN_SHUTTER, PIN_AF, SHUTTER_INVERT);
 Shutter shutter = Shutter(PIN_SHUTTER, SHUTTER_INVERT);
 #endif //USE_AF
 
+Encoder encoder = Encoder(PIN_ENCODER_A, PIN_ENCODER_B);
+MenuItem* currentMenu;
+
+//menus initialisation
+auto mainMenu = MainMenu(&lcd);
+
 
 void setup()
 {
-
+    pinMode(PIN_ENCODER_PRESS, INPUT_PULLUP);
+    pinMode(PIN_CANCEL, INPUT_PULLUP);
+    pinMode(PIN_ENDSTOP, INPUT_PULLUP);
+    encoder.write(0);
+    int old_position = 0;
+    int new_position = 0;
+    currentMenu = &mainMenu;
+    currentMenu.show();
 }
 
 void loop()
 {
+    new_position = encoder.read()/4;
+    while(new_position < old_position)
+    {
+        currentMenu = currentMenu.action_right();
+        new_position++;
+    }
+    while(new_position > old_position)
+    {
+        currentMenu = currentMenu.action_left();
+        new_position--;
+    }
+    encoder.write(0)
+    if(digitalRead(PIN_ENCODER_PRESS, LOW))
+    {
+        currentMenu = currentMenu.action_press();
+    }
+    if (digitalRead(PIN_CANCEL, LOW))
+    {
+        currrentMenu = currentMenu.action_cancel();
+    }
 
 }
---
+++ menu.h
@@ -0,0 +1,74 @@
+#ifndef menu_h
+#define menu_h
+#include "lcd16x2.h"
+class MenuItem
+{
+private:
+    /* data */
+public:
+    String name;
+    Display* lcd;
+    Encoder* encoder;
+    MenuItem* next;
+    MenuItem* prev;
+    MenuItem* up;
+    virtual MenuItem* show() = 0;
+    virtual MenuItem* action_right() = 0;
+    virtual MenuItem* action_left() = 0;
+    virtual MenuItem* action_press() = 0;
+    virtual MenuItem* action_cancel() = 0;
+    
+};
+
+
+//MAIN MENU
+
+class MainMenu : MenuItem
+{
+public:
+
+    MainMenu(Display display): lcd(display)
+    {   
+        next = this;
+        prev = this;
+        up = this;
+        name = "MENU"
+    }
+
+    MenuItem* item = nullptr; //pointer to first item 
+
+    MenuItem* show()
+    {
+        lcd->writeTopLine(">" + item->name);
+        lcd->writeBottomLine(" " + item->next->name);
+        lcd->update();
+        return this;
+    }
+
+    MenuItem* action_right()
+    {
+        item = item->next;
+        show();
+        return this;
+    }
+
+    MenuItem* action_left()
+    {
+        item = item->prev;
+        show();
+        return this;
+    }
+
+    MenuItem* action_press()
+    {
+        return item->show();
+    }
+    MenuItem* action_cancel();
+    {
+        return up->show();
+    }
+};
+
+
+
+#endif menu_h
