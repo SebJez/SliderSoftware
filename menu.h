e30cef14f7964e41de658b77a27ac22a9cbc014d
SebJez <sjezierski123@gmail.com>
2019-06-03 18:43:29 +0200

    a

--- menu.h
+++ menu.h
@@ -20,7 +20,55 @@ public:
     
 };
 
-#include "main_menu.h"// MAIN MENU
+
+//MAIN MENU
+
+class MainMenu : MenuItem
+{
+public:
+
+    MainMenu(Display display): lcd(display)
+    {   
+        next = this;
+        prev = this;
+        up = this;
+        name = T_MAIN_MENU;
+    }
+
+    MenuItem* item = nullptr; //pointer to first item 
+
+    MenuItem* show()
+    {
+        lcd->writeTopLine(">" + item->name);
+        lcd->writeBottomLine(" " + item->next->name);
+        lcd->update();
+        return this;
+    }
+
+    MenuItem* action_right()
+    {
+        item = item->next;
+        show();
+        return this;
+    }
+
+    MenuItem* action_left()
+    {
+        item = item->prev;
+        show();
+        return this;
+    }
+
+    MenuItem* action_press()
+    {
+        return item->show();
+    }
+    MenuItem* action_cancel();
+    {
+        return up->show();
+    }
+};
+
 
 
 #endif menu_h
