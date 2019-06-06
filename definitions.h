4dc4515587e65790c4210df42b109edb27a84f10
SebJez <sjezierski123@gmail.com>
2019-06-07 00:48:01 +0200

    add pins

--- definitions.h
+++ definitions.h
@@ -7,12 +7,15 @@
 
 #ifdef REMOTE_SHUTTER
     #define SHUTTER_INVERT false //set to true if shutter triggers on LOW
-    #define USE_AF
 #endif //REMOTE_SHUTTER
 
 #define USB_SERIAL
+<<<<<<< HEAD
 #define ENCODER
 
+=======
+#define REMOTE_SHUTTER
+>>>>>>> a34a8ee... added pins
 #define LCD_16x2
 //#define LCD_BACKLIGHT
 #ifdef LCD_BACKLIGHT
@@ -31,6 +34,7 @@
 //---------------------------------------------------------------
 //--PINS
 //---------------------------------------------------------------
+<<<<<<< HEAD
 #ifdef  ENCODER
     #define PIN_ENCODER_A
     #define PIN_ENCODER_B
@@ -39,14 +43,21 @@
 
 #define PIN_CANCEL
 #define PIN_ENDSTOP
+=======
+#define PIN_ENCODER_A 6
+#define PIN_ENCODER_B 5
+#define PIN_ENCODER_PRESS 4
+#define PIN_CANCEL 3
+#define PIN_ENDSTOP 2
+>>>>>>> a34a8ee... added pins
 
 #ifdef LCD_16x2
-    #define PIN_LCD_RS 1
-    #define PIN_LCD_E  2
-    #define PIN_LCD_D4 4
-    #define PIN_LCD_D5 3
-    #define PIN_LCD_D6 5
-    #define PIN_LCD_D7 6
+    #define PIN_LCD_RS 7
+    #define PIN_LCD_E  8
+    #define PIN_LCD_D4 9
+    #define PIN_LCD_D5 10
+    #define PIN_LCD_D6 11
+    #define PIN_LCD_D7 12
 #endif //LCD_16x2
 
 #ifdef LCD_BACKLIGHT
@@ -54,9 +65,11 @@
 #endif //PIN_LCD_BACKLIGHT
 
 #ifdef REMOTE_SHUTTER
-    #define PIN_SHUTTER
+    #define PIN_SHUTTER A1
+    #define PIN_AF  A0
 #endif //REMOTE_SHUTTER
 
+<<<<<<< HEAD
 #ifdef USE_AF
     #define PIN_AF
 #endif
@@ -75,3 +88,9 @@
 #endif //ENCODER
 
 #endif //definitions_h
+=======
+#define PIN_STEPPER_A1 A3
+#define PIN_STEPPER_A2 A2
+#define PIN_STEPPER_B1 A5
+#define PIN_STEPPER_B2 A4
+>>>>>>> a34a8ee... added pins
